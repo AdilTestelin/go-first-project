5f87b0760f962611900b2867b40ce25a684785e7
AdilTestelin <adil.testelin59@hotmail.fr>
2024-04-28 07:13:28 +0200

    feat(array): finalize step of arrays kata

--- array/sum.go
+++ array/sum.go
@@ -7,3 +7,29 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sumOfNumbers []int
+
+	for _, arr := range numbersToSum {
+		sumOfNumbers = append(sumOfNumbers, Sum(arr))
+	}
+
+	return sumOfNumbers
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var result []int
+
+	for _, arr := range numbersToSum {
+		arrayLength := len(arr)
+		if arrayLength == 0 {
+			result = append(result, 0)
+		} else {
+			tail := arr[1:]
+			result = append(result, Sum(tail))
+		}
+	}
+
+	return result
+}
